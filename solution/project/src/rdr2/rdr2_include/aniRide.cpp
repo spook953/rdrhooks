5bdf8ce9f6230f2999eb261bcf129d1d9f261c99
spook953 <lttomas953@gmail.com>
2024-11-09 01:15:24 +0200

    Update aniRide.cpp

--- solution/project/src/rdr2/rdr2_include/aniRide.cpp
+++ solution/project/src/rdr2/rdr2_include/aniRide.cpp
@@ -2,5 +2,5 @@
 
 void rdr2::aniRide::FastDismount(bool param_1, bool param_2)
 {
-	sigs::aniRide_FastDismount.call<void>(param_1, param_2);
+	sigs::aniRide_FastDismount.call<void>(this, param_1, param_2);
 }
